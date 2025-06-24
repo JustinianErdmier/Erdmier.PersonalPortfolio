f32573fbc150d6d5cd45b16ab119044958ce6f3f
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-24 16:36:02 -0500

    feat(🚧): wip

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -2,6 +2,14 @@
     setYearsOfExperienceStat();
 });
 
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+                                scrollTop: $($.attr(this, 'href')).offset().top
+                            }, 500);
+});
+
 $(window).scroll(() => {
     const scrolledClass = 'header--scrolled';
     const header = $('#site-header');
